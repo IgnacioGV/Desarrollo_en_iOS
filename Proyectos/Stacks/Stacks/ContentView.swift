1d5faf17f65c5730913915770e223ff3e3d4920b
Jose Ignacio Garcia Velazquez <j.ignaciogv@gmail.com>
2021-04-01 11:34:56 -0600

    Se crea practica 8

---
+++ Proyectos/Stacks/Stacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stacks
+//
+//  Created by Maria Teresa Jimenez on 27/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Proyectos/swiftui/swiftui/swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiApp.swift
+//  swiftui
+//
+//  Created by Maria Teresa Jimenez on 27/02/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
