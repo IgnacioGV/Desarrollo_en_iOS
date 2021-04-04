849b10257ad0746639dce4b8b2df6a4b8d397a51
Jose Ignacio Garcia Velazquez <j.ignaciogv@gmail.com>
2021-04-03 18:16:06 -0600

    Se crea proyecto de Restaurant

---
+++ Proyectos/Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Maria Teresa Jimenez on 03/04/21.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
