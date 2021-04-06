31cce68a8fabf92eb0f084fc6cbb6d560fc28d28
Jose Ignacio Garcia Velazquez <j.ignaciogv@gmail.com>
2021-04-05 22:15:44 -0500

    Se crea proyecto de splashicono

---
+++ Proyectos/splashicono/splashicono/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  splashicono
+//
+//  Created by Maria Teresa Jimenez on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
