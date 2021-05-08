a818f71af13ac01ce2ca8eabdb5c68a0ac03b150
Jose Ignacio Garcia Velazquez <j.ignaciogv@gmail.com>
2021-05-08 13:50:13 -0500

    Se crea proyecto de appbar2

---
+++ Proyectos/tabbar2/tabbar2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tabbar2
+//
+//  Created by Maria Teresa Jimenez on 08/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
